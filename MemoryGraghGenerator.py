e1c2009ab7791c68fa4ad52576930799840c2a97
JaviS-Rei <1477174254@qq.com>
2023-02-24 14:51:16 +0800

    add some assertion and OP_TYEP enum

--- MemoryGraghGenerator.py
+++ MemoryGraghGenerator.py
@@ -3,8 +3,6 @@
 
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-# import matplotlib.ticker as ticker
-from matplotlib.path import Path
 import matplotlib.patches as patches
 
 '''
@@ -21,6 +19,14 @@ log tuple:
     @ BIG: bool, if true, means a PAGE / BIGMEM op
 
 '''
+
+# c type enum def
+MALLOC = 0
+FREE = 1
+MALLOC_PAGE = 2
+FREE_PAGE = 3
+MALLOC_BIGMEM = 4
+FREE_BIGMEM = 5
 
 mem_usage_list = []
 # TODO: find a data structure
@@ -48,35 +54,41 @@ def plot_init():
         axs[i].xaxis.set_ticks([])
         # axs[i].xaxis.set_ticks([mem_start, mem_end])
         # axs[i].xaxis.set_major_formatter(ticker.FormatStrFormatter("0x%x"))
+    fig.suptitle('Memory Usage Graph')
     
 
 
 # read the pre-constructed numpy data
 def update(n):
-    # for i in range(ncpu + 1):
-    #     axs[i].clear()
-    #     axs[i].set_xlim([mem_start, mem_end])
-    #     axs[i].set_ylim([0, 1])
-    #     axs[i].yaxis.set_ticks([])
-    #     axs[i].xaxis.set_ticks([])
-    plt.cla()
-    if n==1:
-        fig.suptitle('Memory Usage Graph')
+    # clear graph before draw
+    for i in range(ncpu + 1):
+        axs[i].clear()
+        axs[i].set_xlim([mem_start, mem_end])
+        axs[i].set_ylim([0, 1])
+        axs[i].yaxis.set_ticks([])
+        axs[i].xaxis.set_ticks([])
     # for i in range(ncpu+1):
     #     axs[i].clear()
     for [CPU, Addr, Size, OP_TYPE, isBIG] in mem_usage_list:
+        assert(mem_start <= Addr <= mem_end)
+        assert(mem_start <= Addr + Size <= mem_end)
         rect = patches.Rectangle((Addr, 0), Size, 1, facecolor='orange')
         axs[CPU+1].add_patch(rect)
-    plt.show()
-        # if isBIG == True:
-        #     axs[0].add_patch(rect)
-    mem_usage_list.append([0, 0x30000000, 0x10000000, 0, True])
+        if isBIG == True:
+            rect = patches.Rectangle((Addr, 0), Size, 1, facecolor='orange')
+            axs[0].add_patch(rect)
+    # # dynamic draw test.
+    # if n==2:
+    #     mem_usage_list.append([0, 0x3000000, 0x1000000, 0, True])
+    # if n==3:
+    #     mem_usage_list.pop(0)
+    
 
 
 if __name__ == '__main__':
     plot_init()
     log_parse()
-    ani = FuncAnimation(fig, update, interval=1000, save_count=100)
+    ani = FuncAnimation(fig, update, interval=10, save_count=100)
 
     # live show
     plt.show()
